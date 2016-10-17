e7f7569f7db103fc9a9b36d69e0e850759ab77f0
Vijay Gaikwad <vig@microsoft.com>
2016-10-16 21:28:14 -0700

    add booksdbcontext

---
+++ Books/Books.Web/DataContexts/BooksDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Books.Entities;
+
+namespace Books.Web.DataContexts
+{
+    public class BooksDb : DbContext
+    {
+        // DbSet property maps to a table in DB.
+        // In this case Books table will be created by Code first
+
+        // We can query the DbSet property with Linq query which are converted to SQL queries by EF.
+        // We can also use this property to insert and deleted objects from the DB.
+
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
